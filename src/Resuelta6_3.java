21456d488b73e07f83a12b377bc1222be9c4ffb4
AngelsGV <angels.not99@gmail.com>
2024-01-13 21:44:00 +0100

    Initial commit

---
+++ src/Resuelta6_3.java
@@ -0,0 +1,9 @@
+//Acierta la contraseña. 1. Se introduce una contraseña.
+// 2. Teclear palabras hasta que acierte.
+// 2 versiones:
+// 1. Se dice si las palabras tienen más o menos caracteres.
+// 2. El programa mostrará la longitud de la contraseña y una cadena con
+// los caracteres acertados en sus lugares respectivos y asteriscos en los no acertados.
+
+public class Resuelta6_3 {
+}
