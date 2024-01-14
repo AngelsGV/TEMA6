eaf43b2088845692e583de1358b12973850a935d
AngelsGV <angels.not99@gmail.com>
2024-01-15 00:31:30 +0100

    Initial commit

---
+++ src/Actividad6_12.java
@@ -0,0 +1,9 @@
+//Juego del ahorcado.
+//1. Intentar que no se vea la palabra que se escribe
+//2. Se muestren guiones para la cantidad de letras que hay
+//3. El jugador escribirá letras y se irán colocando en sus lugares. Los lugares no acertados seguirán con guiones.
+//4. Se debe mostrar las letras acertadas y las utilizadas.
+//5. El jugador tendrá 7 intentos erróneos
+//6. Finaliza al acertar la palabra o al consumir los 7 errores.
+public class Actividad6_12 {
+}
---
+++ src/Actividad6_14.java
@@ -0,0 +1,8 @@
+//AGENDA
+//1. Crear menú de usuario.
+//1.1. Añadir nuevo contacto (nombre y teléfono)
+//1.2. Buscar el teléfono de un contacto con un nombre
+//1.3. Mostrar la información de todos los contactos ordenados mediante el nombre.
+//(Nombre y tel se pueden codificar como una única cadena "nombre:telefono")
+public class Actividad6_14 {
+}
